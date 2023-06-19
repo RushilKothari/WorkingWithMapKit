d22261d47260d3749a9fb388642d87025a8e21f3
RushilKothari <rushilkothari25.rk@gmail.com>
2023-06-19 11:23:50 +0530

    Initial Commit

---
+++ WorkingWithMapKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WorkingWithMapKit
+//
+//  Created by Rushil Kothari on 19/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkingWithMapKit/WorkingWithMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithMapKitApp.swift
+//  WorkingWithMapKit
+//
+//  Created by Rushil Kothari on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
